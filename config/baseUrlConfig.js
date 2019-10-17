21b50398c559e4e5ff0b307b936179a146409cbe
fengying <huangjun_web@163.com>
2019-10-17 08:57:02 +0800

    修改接口基础路径配置

--- config/baseUrlConfig.js
+++ config/baseUrlConfig.js
@@ -2,9 +2,19 @@
 
 const http = window.location.protocol;
 // console.log('$envType: ', $envType);
+let type = 0;
+if (process.env.NODE_ENV === 'development') {
+  type = 0;
+} else if (process.env.NODE_ENV === 'production') {
+  if (location.href.includes('sit')) {
+    type = 1;
+  } else {
+    type = 2;
+  }
+}
 module.exports = {
   get baseUrl() {
     let urlTest = [`${http}//www.dev.com`, `${http}//www.sit.com`, `${http}//www.prod.com`];
-    return urlTest[$envType.type];
+    return urlTest[type];
   }
 }
